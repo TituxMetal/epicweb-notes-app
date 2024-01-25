47c4f66d0bd6c93b96644ef2f5c1fa86c5bf2a31
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-25 05:40:59 +0100

    Add NoteRoute component for displaying individual notes

---
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -0,0 +1,27 @@
+import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+import { db, invariantResponse } from '~/utils'
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const note = db.note.findFirst({ where: { id: { equals: params.noteId } } })
+
+  invariantResponse(note, 'Note not found', { status: 404 })
+
+  return json({ note: { title: note.title, content: note.content } })
+}
+
+const NoteRoute = () => {
+  const data = useLoaderData<typeof loader>()
+
+  return (
+    <div className='absolute inset-0 flex flex-col px-10'>
+      <h2 className='mb-2 pt-12 text-h2 lg:mb-6'>{data.note.title}</h2>
+      <div className='overflow-y-auto pb-24'>
+        <p className='whitespace-break-spaces text-sm md:text-lg'>{data.note.content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default NoteRoute
