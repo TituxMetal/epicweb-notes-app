c37775797c37e32f0c53b120d0aa1388a97344b0
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-25 08:38:46 +0100

    Add delete functionality to note route

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -1,7 +1,18 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { json, redirect, type ActionFunctionArgs, type LoaderFunctionArgs } from '@remix-run/node'
+import { Form, Link, useLoaderData } from '@remix-run/react'
 
 import { db, invariantResponse } from '~/utils'
+
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+  const intent = formData.get('intent')
+
+  invariantResponse(intent === 'delete', 'Invalid intent')
+
+  db.note.delete({ where: { id: { equals: params.noteId } } })
+
+  return redirect(`/users/${params.username}/notes`)
+}
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const note = db.note.findFirst({ where: { id: { equals: params.noteId } } })
@@ -21,9 +32,16 @@ const NoteRoute = () => {
         <p className='whitespace-break-spaces text-sm md:text-lg'>{data.note.content}</p>
       </div>
       <div className='absolute bottom-3 left-3 right-3 flex items-center justify-end gap-2 rounded-lg bg-sky-600/80 p-4 pl-5  backdrop-blur-sm md:gap-4 md:pl-7'>
-        <button className='rounded-md bg-rose-700 px-4 py-2 text-lg font-semibold text-gray-50 hover:bg-rose-800 focus:outline-none focus:ring-2 focus:ring-rose-700 focus:ring-offset-2 focus:ring-offset-sky-600/80 lg:w-auto'>
-          Delete
-        </button>
+        <Form method='post'>
+          <button
+            type='submit'
+            name='intent'
+            value='delete'
+            className='rounded-md bg-rose-700 px-4 py-2 text-lg font-semibold text-gray-50 hover:bg-rose-800 focus:outline-none focus:ring-2 focus:ring-rose-700 focus:ring-offset-2 focus:ring-offset-sky-600/80 lg:w-auto'
+          >
+            Delete
+          </button>
+        </Form>
         <button className='rounded-md bg-gray-700 px-4 py-2 text-lg font-semibold text-gray-50 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-sky-600/80 lg:w-auto'>
           <Link to='edit'>Edit</Link>
         </button>
