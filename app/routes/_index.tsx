139edba8c8727ff6c5ad3b69561853c21bd046a4
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-25 05:34:56 +0100

    Refactor index route component

--- app/routes/_index.tsx
+++
@@ -1,8 +0,0 @@
-const IndexPage = () => {
-  return (
-    <>
-      <h1 className='m-auto text-2xl font-extrabold text-sky-500'>Welcome to Epic Web Notes App</h1>
-    </>
-  )
-}
-export default IndexPage
