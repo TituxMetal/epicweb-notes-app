6c1c7a9b9f8cc7c71ae8488ac9e9c715632bdc06
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-25 11:40:11 +0100

    Add prefetch intent to notes link

--- app/routes/users+/$username.tsx
+++ app/routes/users+/$username.tsx
@@ -17,7 +17,7 @@ const ProfileRoute = () => {
   return (
     <div className='container mb-48 mt-36'>
       <h1 className='text-h1'>{data.user.name ?? data.user.username}</h1>
-      <Link to='notes' className='underline'>
+      <Link to='notes' prefetch='intent' className='underline'>
         Notes
       </Link>
     </div>
