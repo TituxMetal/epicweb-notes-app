fc3933246fe42e1708018cbd03c9c44f07665043
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-25 05:33:30 +0100

    Add container styles and custom font sizes to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,28 @@ export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['Nunito Sans', 'Nunito Sans Fallback', ...defaultTheme.fontFamily.sans]
       },
       colors: {
         gray: colors.zinc
+      },
+      fontSize: {
+        // 1rem = 16px
+        /** 80px size / 84px high / bold */
+        mega: ['5rem', { lineHeight: '5.25rem', fontWeight: '700' }],
+        /** 56px size / 62px high / bold */
+        h1: ['3.5rem', { lineHeight: '3.875rem', fontWeight: '700' }],
+        /** 40px size / 48px high / bold */
+        h2: ['2.5rem', { lineHeight: '3rem', fontWeight: '700' }]
       }
     }
   },
