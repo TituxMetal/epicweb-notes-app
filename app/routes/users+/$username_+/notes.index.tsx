9141c8477ef324344da11a92322f0674780c17b1
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-25 05:40:18 +0100

    Add NotesIndexRoute component

---
+++ app/routes/users+/$username_+/notes.index.tsx
@@ -0,0 +1,9 @@
+const NotesIndexRoute = () => {
+  return (
+    <div className='container pt-12'>
+      <p className='text-body-md'>Select a note</p>
+    </div>
+  )
+}
+
+export default NotesIndexRoute
