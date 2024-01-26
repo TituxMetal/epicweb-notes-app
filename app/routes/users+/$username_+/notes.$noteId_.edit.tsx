af8565e23aa12b2f6555c2d0f538c3281360a159
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-26 06:01:42 +0100

    Add floating toolbar, replace form fields and buttons with custom components in NoteEditRoute

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,7 +1,8 @@
 import { json, redirect, type ActionFunctionArgs, type LoaderFunctionArgs } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 
-import { db, invariantResponse } from '~/utils'
+import { Button, FormField, floatingToolbarClassName } from '~/components'
+import { db, invariantResponse, useIsSubmitting } from '~/utils'
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
   const formData = await request.formData()
@@ -26,43 +27,26 @@ export const loader = ({ params }: LoaderFunctionArgs) => {
 
 const NoteEditRoute = () => {
   const data = useLoaderData<typeof loader>()
+  const isSubmitting = useIsSubmitting()
 
   return (
     <Form method='post' className='flex h-full flex-col gap-y-4 overflow-x-hidden px-8 py-12'>
       <fieldset className='flex flex-col gap-4 rounded-lg bg-sky-700 p-4'>
-        <div className='flex flex-col gap-1'>
-          <label htmlFor='title'>Title</label>
-          <input
-            type='text'
-            name='title'
-            id='title'
-            className='rounded-md border-sky-500 bg-sky-900 text-gray-100 focus:ring-sky-600 focus:ring-offset-gray-900'
-            defaultValue={data.note.title}
-          />
-        </div>
-        <div className='flex flex-col gap-1'>
-          <label htmlFor='content'>Content</label>
-          <textarea
-            id='content'
-            name='content'
-            required
-            className='min-h-40 rounded-md border-sky-500 bg-sky-900 text-gray-100 focus:ring-sky-600 focus:ring-offset-gray-900'
-            defaultValue={data.note.content}
-          />
-        </div>
-        <div className='absolute bottom-3 left-3 right-3 flex items-center justify-end gap-2 rounded-lg bg-sky-600/80 p-4 pl-5 backdrop-blur-sm md:gap-4 md:pl-7'>
-          <button
-            type='reset'
-            className='rounded-md bg-rose-700 px-4 py-2 text-lg font-semibold text-gray-50 hover:bg-rose-800 focus:outline-none focus:ring-2 focus:ring-rose-700 focus:ring-offset-2 focus:ring-offset-sky-600/80 lg:w-auto'
-          >
+        <FormField>
+          <FormField.Label htmlFor='title'>Title</FormField.Label>
+          <FormField.Input type='text' name='title' id='title' defaultValue={data.note.title} />
+        </FormField>
+        <FormField>
+          <FormField.Label htmlFor='content'>Content</FormField.Label>
+          <FormField.Textarea name='content' id='content' defaultValue={data.note.content} />
+        </FormField>
+        <div className={floatingToolbarClassName}>
+          <Button type='reset' intent='destructive'>
             Reset
-          </button>
-          <button
-            type='submit'
-            className='rounded-md bg-gray-700 px-4 py-2 text-lg font-semibold text-gray-50 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-sky-600/80 lg:w-auto'
-          >
-            Submit
-          </button>
+          </Button>
+          <Button disabled={isSubmitting} type='submit' intent='base'>
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </Button>
         </div>
       </fieldset>
     </Form>
