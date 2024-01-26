fa1aff9234998e893d4999af62a2694030a5cbc0
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-26 05:58:23 +0100

    Add useIsSubmitting hook to misc.ts

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,3 @@
 export { db } from './db.server'
 export { invariant, invariantResponse } from './invariantResponse'
-export { cn } from './misc'
+export { cn, useIsSubmitting } from './misc'
--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -1,3 +1,4 @@
+import { useFormAction, useNavigation } from '@remix-run/react'
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
@@ -7,4 +8,24 @@ import { twMerge } from 'tailwind-merge'
  */
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * Returns true if the current navigation is submitting the current route's
+ * form. Defaults to the current route's form action and method POST.
+ */
+export const useIsSubmitting = ({
+  formAction,
+  formMethod = 'POST'
+}: {
+  formAction?: string
+  formMethod?: 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE'
+} = {}) => {
+  const contextualFormAction = useFormAction()
+  const navigation = useNavigation()
+  return (
+    navigation.state === 'submitting' &&
+    navigation.formAction === (formAction ?? contextualFormAction) &&
+    navigation.formMethod === formMethod
+  )
 }
