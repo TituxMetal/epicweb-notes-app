86b445abc08e5c376114f8d829af5c0ed7e178d7
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-26 18:29:38 +0100

    Remove unused hooks

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,4 +1,4 @@
 export { db } from './db.server'
-export { useFocusInvalid, useHydrated, useIsSubmitting } from './hooks'
+export { useIsSubmitting } from './hooks'
 export { invariant, invariantResponse } from './invariantResponse'
 export { cn, getErrorMessage } from './misc'
