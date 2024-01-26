c8e22e2d027edbaf313fad7d37cd4a6b423bbf0e
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-26 09:34:29 +0100

    Add form validation and display form and fields errors in note edit route

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,8 +1,25 @@
 import { json, redirect, type ActionFunctionArgs, type LoaderFunctionArgs } from '@remix-run/node'
-import { Form, useLoaderData } from '@remix-run/react'
+import { Form, useActionData, useLoaderData } from '@remix-run/react'
 
-import { Button, FormField, GeneralErrorBoundary, floatingToolbarClassName } from '~/components'
-import { db, invariantResponse, useIsSubmitting } from '~/utils'
+import {
+  Button,
+  ErrorList,
+  FormField,
+  GeneralErrorBoundary,
+  floatingToolbarClassName
+} from '~/components'
+import { db, invariantResponse, useHydrated, useIsSubmitting } from '~/utils'
+
+type ActionError = {
+  formErrors: Array<string>
+  fieldErrors: {
+    title: Array<string>
+    content: Array<string>
+  }
+}
+
+const titleMaxLength = 100
+const contentMaxLength = 1000
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
   const formData = await request.formData()
@@ -11,6 +28,38 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
 
   invariantResponse(typeof title === 'string', 'Title must be a string')
   invariantResponse(typeof content === 'string', 'Content must be a string')
+
+  const errors: ActionError = {
+    formErrors: [],
+    fieldErrors: {
+      title: [],
+      content: []
+    }
+  }
+
+  if (title === '') {
+    errors.fieldErrors.title.push('Title is required')
+  }
+
+  if (title.length > titleMaxLength) {
+    errors.fieldErrors.title.push(`Title must be ${titleMaxLength} characters or less`)
+  }
+
+  if (content === '') {
+    errors.fieldErrors.content.push('Content is required')
+  }
+
+  if (content.length > contentMaxLength) {
+    errors.fieldErrors.content.push(`Content must be ${contentMaxLength} characters or less`)
+  }
+
+  const hasErrors =
+    errors.formErrors.length ||
+    Object.values(errors.fieldErrors).some(fieldErrors => fieldErrors.length)
+
+  if (hasErrors) {
+    return json({ status: 'error', errors } as const, { status: 400 })
+  }
 
   db.note.update({ where: { id: { equals: params.noteId } }, data: { title, content } })
 
@@ -27,19 +76,42 @@ export const loader = ({ params }: LoaderFunctionArgs) => {
 
 const NoteEditRoute = () => {
   const data = useLoaderData<typeof loader>()
+  const actionData = useActionData<typeof action>()
   const isSubmitting = useIsSubmitting()
 
+  const fieldErrors = actionData?.status === 'error' ? actionData.errors.fieldErrors : null
+  const formErrors = actionData?.status === 'error' ? actionData.errors.formErrors : null
+  const isHydrated = useHydrated()
+
   return (
-    <Form method='post' className='flex h-full flex-col gap-y-4 overflow-x-hidden px-8 py-12'>
+    <Form
+      method='post'
+      noValidate={isHydrated}
+      className='flex h-full flex-col gap-y-4 overflow-x-hidden px-8 py-12'
+    >
       <fieldset className='flex flex-col gap-4 rounded-lg bg-sky-700 p-4'>
         <FormField>
           <FormField.Label htmlFor='title'>Title</FormField.Label>
-          <FormField.Input type='text' name='title' id='title' defaultValue={data.note.title} />
+          <FormField.Input
+            type='text'
+            name='title'
+            id='title'
+            defaultValue={data.note.title}
+            maxLength={titleMaxLength}
+          />
+          {fieldErrors?.title && <ErrorList errors={fieldErrors.title} />}
         </FormField>
         <FormField>
           <FormField.Label htmlFor='content'>Content</FormField.Label>
-          <FormField.Textarea name='content' id='content' defaultValue={data.note.content} />
+          <FormField.Textarea
+            name='content'
+            id='content'
+            defaultValue={data.note.content}
+            maxLength={contentMaxLength}
+          />
+          {fieldErrors?.content && <ErrorList errors={fieldErrors.content} />}
         </FormField>
+        {formErrors && <ErrorList errors={formErrors} />}
         <div className={floatingToolbarClassName}>
           <Button type='reset' intent='destructive'>
             Reset
