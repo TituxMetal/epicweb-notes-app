03e7d24bf6a737715f79660e6c8d3da546a2ff57
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-26 06:48:14 +0100

    Add dynamic meta tags with data coming from parent route

--- app/routes/users+/$username_+/notes.$noteId.tsx
+++ app/routes/users+/$username_+/notes.$noteId.tsx
@@ -1,8 +1,34 @@
-import { json, redirect, type ActionFunctionArgs, type LoaderFunctionArgs } from '@remix-run/node'
+import {
+  json,
+  redirect,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  type MetaFunction
+} from '@remix-run/node'
 import { Form, Link, useLoaderData } from '@remix-run/react'
 
 import { Button, floatingToolbarClassName } from '~/components'
 import { db, invariantResponse } from '~/utils'
+
+import { type loader as notesLoader } from './notes'
+
+export const meta: MetaFunction<
+  typeof loader,
+  { 'routes/users+/$username_+/notes': typeof notesLoader }
+> = ({ data, params, matches }) => {
+  const notesMatch = matches.find(m => m.id === 'routes/users+/$username_+/notes')
+  const displayName = notesMatch?.data?.owner.name ?? params.username
+  const noteTitle = data?.note.title ?? 'Note'
+  const noteContentsSummary =
+    data && data.note.content.length > 100 ? data?.note.content.slice(0, 97) + '...' : 'No content'
+  return [
+    { title: `${noteTitle} | ${displayName}'s Notes | Epic Notes` },
+    {
+      name: 'description',
+      content: noteContentsSummary
+    }
+  ]
+}
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
   const formData = await request.formData()
--- app/routes/users+/$username_+/notes.index.tsx
+++ app/routes/users+/$username_+/notes.index.tsx
@@ -1,3 +1,24 @@
+import { type MetaFunction } from '@remix-run/node'
+
+import { type loader as notesLoader } from './notes'
+
+export const meta: MetaFunction<
+  null,
+  { 'routes/users+/$username_+/notes': typeof notesLoader }
+> = ({ params, matches }) => {
+  const notesMatch = matches.find(m => m.id === 'routes/users+/$username_+/notes')
+  const displayName = notesMatch?.data?.owner.name ?? params.username
+  const noteCount = notesMatch?.data?.notes.length ?? 0
+  const notesText = noteCount === 1 ? 'note' : 'notes'
+  return [
+    { title: `${displayName}'s Notes | Epic Notes` },
+    {
+      name: 'description',
+      content: `Checkout ${displayName}'s ${noteCount} ${notesText} on Epic Notes`
+    }
+  ]
+}
+
 const NotesIndexRoute = () => {
   return (
     <div className='container pt-12'>
