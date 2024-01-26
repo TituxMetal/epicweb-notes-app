88ea44a027a71d94990bb8faaa1b3e211379e16b
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-26 08:15:01 +0100

    Add GeneralErrorBoundary component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { Button, type ButtonProps } from './Button'
 export { FormField } from './FormField'
+export { GeneralErrorBoundary } from './GeneralErrorBoundary'
 export { floatingToolbarClassName } from './floating-toolbar'
