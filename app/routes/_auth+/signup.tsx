52a05dce9f087b1574254cf351fca681b9ec0dd9
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-27 20:20:22 +0100

    Add signup route component to play with honeypot

---
+++ app/routes/_auth+/signup.tsx
@@ -0,0 +1,43 @@
+import { redirect, type ActionFunctionArgs, type MetaFunction } from '@remix-run/node'
+import { Form } from '@remix-run/react'
+import { HoneypotInputs } from 'remix-utils/honeypot/react'
+
+import { Button, FormField } from '~/components'
+import { checkHoneypot } from '~/utils'
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'Setup Epic Notes Account' }]
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+
+  checkHoneypot(formData)
+
+  return redirect('/')
+}
+
+const SignupRoute = () => {
+  return (
+    <section className='container mx-auto flex min-h-full max-w-screen-lg flex-col justify-center px-8'>
+      <div className='mx-auto flex min-h-full w-full flex-1 flex-col items-center justify-center rounded-xl bg-sky-800'>
+        <div className='flex flex-col gap-3 text-center'>
+          <h1 className='text-h1'>Welcome aboard!</h1>
+          <p className='text-base text-gray-200'>Please enter your details.</p>
+        </div>
+        <Form method='POST' className='mx-auto flex min-w-80 max-w-sm flex-col gap-4'>
+          <HoneypotInputs />
+          <FormField>
+            <FormField.Label htmlFor='email-input'>Email</FormField.Label>
+            <FormField.Input autoFocus id='email-input' name='email' type='email' />
+          </FormField>
+          <Button className='w-full' type='submit'>
+            Create an account
+          </Button>
+        </Form>
+      </div>
+    </section>
+  )
+}
+
+export default SignupRoute
