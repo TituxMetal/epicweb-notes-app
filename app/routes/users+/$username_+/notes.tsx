63227ecf1552d4d0791b6cb52c565081297b8729
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-28 20:52:07 +0100

    Refactor user notes loader to use Prisma and add user profile image

--- app/routes/users+/$username_+/notes.tsx
+++ app/routes/users+/$username_+/notes.tsx
@@ -2,18 +2,22 @@ import { json, type LoaderFunctionArgs } from '@remix-run/node'
 import { Link, NavLink, Outlet, useLoaderData } from '@remix-run/react'
 
 import { GeneralErrorBoundary } from '~/components'
-import { cn, db, invariantResponse } from '~/utils'
+import { cn, getUserImgSrc, invariantResponse, prisma } from '~/utils'
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const owner = db.user.findFirst({ where: { username: { equals: params.username } } })
+  const owner = await prisma.user.findUnique({
+    select: {
+      name: true,
+      username: true,
+      image: { select: { id: true } },
+      notes: { select: { id: true, title: true } }
+    },
+    where: { username: params.username }
+  })
 
   invariantResponse(owner, 'Owner not found', { status: 404 })
-
-  const notes = db.note
-    .findMany({ where: { owner: { username: { equals: params.username } } } })
-    .map(({ id, title }) => ({ id, title }))
-
-  return json({ owner, notes })
+  console.log('owner', { owner, notes: owner.notes })
+  return json({ owner })
 }
 
 const NotesRoute = () => {
@@ -27,13 +31,18 @@ const NotesRoute = () => {
       <div className='grid w-full grid-cols-4 bg-sky-900 pl-2 md:container md:mx-2 md:rounded-3xl md:pr-0'>
         <div className='relative col-span-1'>
           <section className='absolute inset-0 flex flex-col'>
-            <Link to='..' relative='path' className='pb-4 pl-8 pr-4 pt-12'>
+            <Link to='..' relative='path' className='pb-4 pl-4 pr-2 pt-12'>
+              <img
+                src={getUserImgSrc(data.owner.image?.id)}
+                alt={ownerDisplayName}
+                className='size-20 object-cover lg:size-32'
+              />
               <h1 className='text-base font-bold md:text-lg lg:text-left lg:text-2xl'>
                 {ownerDisplayName}'s Notes
               </h1>
             </Link>
             <ul className='overflow-y-auto overflow-x-hidden pb-12'>
-              {data.notes.map(note => (
+              {data.owner.notes.map(note => (
                 <li key={note.id} className='p-1 pr-0'>
                   <NavLink
                     to={note.id}
