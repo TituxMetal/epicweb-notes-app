905187ca2d63e9e6270ebe5aee5f79b0dfe9c644
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-28 20:45:40 +0100

    Refactor image loading routes Add a resources route for user images and note images

---
+++ app/routes/resources+/note-images.$imageId.tsx
@@ -0,0 +1,24 @@
+import { type LoaderFunctionArgs } from '@remix-run/node'
+
+import { invariantResponse, prisma } from '~/utils'
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariantResponse(params.imageId, 'Image ID is required.', { status: 400 })
+
+  const image = await prisma.noteImage.findUnique({
+    where: { id: params.imageId },
+    select: { contentType: true, blob: true }
+  })
+  console.log('image', image)
+
+  invariantResponse(image, 'Image not found', { status: 404 })
+
+  return new Response(image.blob, {
+    headers: {
+      'content-type': image.contentType,
+      'content-length': Buffer.byteLength(image.blob).toString(),
+      'content-disposition': `inline; filename="${params.imageId}"`,
+      'cache-control': 'public, max-age=31536000, immutable'
+    }
+  })
+}
