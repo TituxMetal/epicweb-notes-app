ca9fe611a8acd6c25c51ee794c0d6228e169162b
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-03 04:57:32 +0100

    Add Toaster component and ShowToast component

--- app/root.tsx
+++ app/root.tsx
@@ -22,15 +22,29 @@ import os from 'node:os'
 import { type ReactNode } from 'react'
 import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { HoneypotProvider } from 'remix-utils/honeypot/react'
+import { Toaster } from 'sonner'
 
 import faviconAssetUrl from '~/assets/favicon.svg'
+import {
+  GeneralErrorBoundary,
+  SearchBar,
+  ShowToast,
+  ThemeFormSchema,
+  ThemeSwitch
+} from '~/components'
 import fontStylesheetUrl from '~/styles/font.css'
 import tailwindStylesheetLink from '~/styles/tailwind.css'
-
-import { GeneralErrorBoundary, SearchBar, ThemeFormSchema, ThemeSwitch } from './components'
-import { type Theme } from './types'
-import { csrf, getTheme, honeypot, invariantResponse, setTheme } from './utils'
-import { useTheme } from './utils/hooks'
+import { type Theme } from '~/types'
+import {
+  combineHeaders,
+  csrf,
+  getTheme,
+  getToast,
+  honeypot,
+  invariantResponse,
+  setTheme,
+  useTheme
+} from '~/utils'
 
 export const links: LinksFunction = () => {
   return [
@@ -50,13 +64,17 @@ export const meta: MetaFunction = () => {
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData()
+
   invariantResponse(formData.get('intent') === 'update-theme', 'Invalid intent', { status: 400 })
+
   const submission = parse(formData, {
     schema: ThemeFormSchema
   })
+
   if (submission.intent !== 'submit') {
     return json({ status: 'success', submission } as const)
   }
+
   if (!submission.value) {
     return json({ status: 'error', submission } as const, { status: 400 })
   }
@@ -70,10 +88,22 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const honeyProps = honeypot.getInputProps()
   const [csrfToken, csrfCookieHeader] = await csrf.commitToken(request)
+  const { toast, headers: toastHeaders } = await getToast(request)
 
   return json(
-    { username: os.userInfo().username, theme: getTheme(request), honeyProps, csrfToken },
-    { headers: csrfCookieHeader ? { 'set-cookie': csrfCookieHeader } : {} }
+    {
+      username: os.userInfo().username,
+      theme: getTheme(request),
+      toast,
+      honeyProps,
+      csrfToken
+    },
+    {
+      headers: combineHeaders(
+        csrfCookieHeader ? { 'set-cookie': csrfCookieHeader } : null,
+        toastHeaders
+      )
+    }
   )
 }
 
@@ -88,6 +118,7 @@ const Document = ({ children, theme }: { children: ReactNode; theme: Theme }) =>
       </head>
       <body className='flex h-full flex-col justify-between bg-gray-800 text-gray-50'>
         {children}
+        <Toaster position='top-center' />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
@@ -98,7 +129,7 @@ const Document = ({ children, theme }: { children: ReactNode; theme: Theme }) =>
 
 const App = () => {
   const data = useLoaderData<typeof loader>()
-  const theme = data.theme
+  const theme = useTheme()
   const matches = useMatches()
   const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
 
@@ -131,6 +162,7 @@ const App = () => {
         <p>Built with ♥️ by {data.username}</p>
         <ThemeSwitch userPreference={theme} />
       </div>
+      {data.toast ? <ShowToast toast={data.toast} /> : null}
     </Document>
   )
 }
